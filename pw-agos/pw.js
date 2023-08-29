75d442168b272c6aee4e9f13ec2bfcca15715b62
Tiago Nuñez <Tnnunez05@gmail.com>
2023-08-29 16:03:14 -0300

    ejercicio 6

--- pw-agos/pw.js
+++ pw-agos/pw.js
@@ -1,3 +1,4 @@
+/* ACTIVIDAD 5 
 let numeros = [-5, -9, -28, -17, -6];
 
 function sortAbsoluteNumbers(copiaNumeros) {
@@ -9,3 +10,27 @@ function sortAbsoluteNumbers(copiaNumeros) {
   return arr.sort((a, b) => a - b);
 }
 console.log(sortAbsoluteNumbers(numeros));
+*/
+//ACTIVIDAD 6
+words = [
+  "hola",
+  "tres",
+  "uno",
+  "Universidad",
+  "de",
+  "Buenos Aires",
+  "pepito",
+  "celular",
+];
+word = "pepito";
+function buscaPalabras(words, word) {
+  let nom = [];
+  let indice = words.indexOf(word);
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > indice) {
+      nom.push(words[i]);
+    }
+  }
+  console.log(nom);
+}
+buscaPalabras(words, word);
