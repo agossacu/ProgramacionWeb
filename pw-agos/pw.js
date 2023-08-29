3d32fa744193d81de5121fbe969692ddfc487c02
Tiago Nuñez <Tnnunez05@gmail.com>
2023-08-29 17:11:48 -0300

    ejercicios 1 y 2 incompleto de objetos

--- pw-agos/pw.js
+++ pw-agos/pw.js
@@ -11,7 +11,7 @@ function sortAbsoluteNumbers(copiaNumeros) {
 }
 console.log(sortAbsoluteNumbers(numeros));
 */
-//ACTIVIDAD 6
+/*ACTIVIDAD 6
 words = [
   "hola",
   "tres",
@@ -34,3 +34,27 @@ function buscaPalabras(words, word) {
   console.log(nom);
 }
 buscaPalabras(words, word);
+*/ /* ACTIVIDAD 1 OBJETOS
+
+function createObject(name, subs) {
+  const persona = {
+    name,
+    subs,
+    hash: name.length * subs,
+    walk: function () {
+      return `El canal de ${name} tiene ${subs} subscriptores.`;
+    },
+  };
+  return persona;
+}
+let botella = createObject("fabry", 2929);
+console.log(botella.walk());
+*/
+/*ACTIVIDAD 2 OBJETO
+
+1)se muestra 70 180, ya que son los valores que se pidieron en el console.log
+2)pw.js:64 Uncaught ReferenceError: age is not defined
+    at pw.js:64:13
+  Da error porque la referencia de eso no existe, referencia son las variables,
+  deberia decir console.log(personAge)
+3)  */
